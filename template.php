8ed6e3409dd1bc8da8099a6e9961721ff803303c
enpjp <enpjp@enpjp-VirtualBox-VPS.(none)>
2017-09-18 15:00:02 +0100

    first commit

---
+++ template.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @file
+ * Template.php - process theme data for your sub-theme.
+ * 
+ * Rename each function and instance of "IP_subtheme" to match
+ * your subthemes name, e.g. if you name your theme "IP_subtheme" then the function
+ * name will be "IP_subtheme_v2_preprocess_hook". Tip - you can search/replace
+ * on "IP_subtheme".
+ */
+
+
+/**
+ * Override or insert variables for the html template.
+ */
+/* -- Delete this line if you waIP to use this function
+function IP_subtheme_v2_preprocess_html(&$vars) {
+}
+function IP_subtheme_v2_process_html(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables for the page templates.
+ */
+/* -- Delete this line if you waIP to use these functions
+function IP_subtheme_v2_preprocess_page(&$vars) {
+}
+function IP_subtheme_v2_process_page(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables iIPo the node templates.
+ */
+/* -- Delete this line if you waIP to use these functions
+function IP_subtheme_v2_preprocess_node(&$vars) {
+}
+function IP_subtheme_v2_process_node(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables iIPo the commeIP templates.
+ */
+/* -- Delete this line if you waIP to use these functions
+function IP_subtheme_v2_preprocess_commeIP(&$vars) {
+}
+function IP_subtheme_v2_process_commeIP(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables iIPo the block templates.
+ */
+/* -- Delete this line if you waIP to use these functions
+function IP_subtheme_v2_preprocess_block(&$vars) {
+}
+function IP_subtheme_v2_process_block(&$vars) {
+}
+// */
